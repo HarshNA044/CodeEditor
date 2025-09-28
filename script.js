8575aa6ea43328cdc687216fbb78f60e92530f0b
Harsh <152859342+HarshNA044@users.noreply.github.com>
2025-09-28 18:43:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+// fn for changing textbox according to button selected by the user
 function codeboxhandler(buttontype) {
+    // firstly we hide all the textbox
     document.getElementById("htmlbox").style.display="none";
     document.getElementById("cssbox").style.display="none";
     document.getElementById("jsbox").style.display="none";
     document.getElementById("htmlbutton").style.backgroundColor="white";
     document.getElementById("cssbutton").style.backgroundColor="white";
     document.getElementById("jsbutton").style.backgroundColor="white";
-
+// now changing the textbox based on condition as selected by the user
     if (buttontype=="htmlbutton") {
         document.getElementById("htmlbox").style.display="block";
         document.getElementById("htmlbutton").style.backgroundColor="yellow";
@@ -19,12 +21,12 @@ function codeboxhandler(buttontype) {
         document.getElementById("jsbutton").style.backgroundColor="red";
     }
 }
-
+// fn that executes the code every time user input in textbox
 function runcode() {
     const htmlcode=document.getElementById('htmlcode').value;
     const csscode=document.getElementById('csscode').value;
-        const jscode=document.getElementById('jscode').value;
+    const jscode=document.getElementById('jscode').value;
 
-const fullcode=htmlcode+"<style>"+csscode+"</style>"+"<script>"+jscode+"</script>";
-    document.getElementById('outputbox').srcdoc=fullcode;
+    const fullcode=htmlcode+"<style>"+csscode+"</style>"+"<script>"+jscode+"</script>"; // putting all html css js reference in a variable
+    document.getElementById('outputbox').srcdoc=fullcode; // now giving the fullcode in refernce of iframe for output
 }
